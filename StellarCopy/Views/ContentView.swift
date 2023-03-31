63ffefbc4391a3ff8775d4b8a6ce2d8e247b018d
Jacob M Sock <jacobmsock@gmail.com>
2023-03-30 23:48:49 -0400

    add Omar's implementation of Talon's design in ContentView

--- StellarCopy/Views/ContentView.swift
+++ StellarCopy/Views/ContentView.swift
@@ -7,20 +7,52 @@
 import SwiftUI
 
 struct ContentView: View {
-    @EnvironmentObject var userViewModel: UserViewModel
-
-    @State private var userIsLoggedIn: Bool = false
+    @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
-        VStack {
-            userIsLoggedIn ? AnyView(MainTabView()) : AnyView(LoginView(userIsLoggedIn: self.$userIsLoggedIn))
+        NavigationStack {
+            VStack(spacing: -10) {
+                
+                VStack {
+                    
+                    TabView {
+                        DailyHoroscopeTextView()
+                            .padding()
+                        ChartView()
+                        LunarEventCapsuleView()
+    //                  MARK: LunarCalView()
+                    }
+                    .tabViewStyle(.page)
+                    
+                }
+            }
+//            .toolbar {
+//                Image(systemName: "gear")
+//                    .foregroundColor(Color(hue: 0.119, saturation: 0.62, brightness: 0.908))
+//            }
+            Spacer()
         }
     }
+    
+// MARK: Code that loops through UIFont families
+//      init() {
+//          for familyName in UIFont.familyNames {
+//              print(familyName)
+//
+//              for fontName in UIFont.fontNames(forFamilyName: familyName) {
+//                  print("-- \(fontName)")
+//              }
+//          }
+//      }
+    
+    
+    
+    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()            .environmentObject(UserViewModel())
-
+        ContentView()
     }
 }
